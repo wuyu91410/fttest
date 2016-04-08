9298f574e76bfcdd8079ee9dd3c29d159c39046c
wuyu91410 <wuyu91410@gmail.com>
2016-04-08 11:18:24 -0400

    Commit
    first commit

---
+++ server.R
@@ -0,0 +1,121 @@
+library(shiny)
+library(pwr)
+library(samplesize)
+#F-test two tail power
+pwr=function(var1,var2,n1,n2,a1){
+	ratio=var1/var2
+	df1=n1-1
+	df2=n2-1
+	f1=qf(1-a1/2,df1,df2,lower.tail=F)
+	f2=qf(a1/2,df1,df2,lower.tail=F)
+	beta=pf(ratio^(-1)*f1,df1,df2,lower.tail=F)-pf(ratio^(-1)*f2,df1,df2,lower.tail=F)
+	power=1-beta
+	return(power)
+}
+
+#F-test two tail required sample size
+sample=function(a,pwr,ratio,ar){
+	for (n in 2:1000){
+		df1=n-1
+		df2=round(ar*df1)
+		if(df2 < 1) df2=1
+		f1=qf(1-a/2,df1,df2,lower.tail=F)
+		f2=qf(a/2,df1,df2,lower.tail=F)
+		beta=pf(ratio^(-1)*f1,df1,df2,lower.tail=F)-pf(ratio^(-1)*f2,df1,df2,lower.tail=F)
+		power=1-beta
+		if (round(power,3) >= pwr) break
+	}
+	n1=n
+	n2=df2+1
+	return(list(n1,n2))
+}
+
+#calculate effective size d
+d=function(mu1,mu2,var1,var2,n1,n2){
+	d=(mu1-mu2)/sqrt((n1-1)*var1+(n2-1)*var2/(n1+n2-2))
+}
+#output
+shinyServer(function(input, output) {
+        output$text1 <- renderText({
+            paste("Using the Contractor's Risk,",paste(tags$i(HTML("&alpha;")),",",sep=""),"of",input$a1,"with the sample size given, the risk of the State DOT not detecting a difference between the variances of State DOT's and Contractor's sample data when one exists,",paste(tags$i(HTML("&beta;")),",",sep=""),"is",paste(tags$code(1-round(pwr(input$var1,input$var2,input$n1,input$n2,as.numeric(input$a1)),4)),".",sep=""),"Thus the power of the test, (1-",HTML(paste(tags$i(HTML("&beta;")),")",sep="")),"is",paste(tags$code(round(pwr(input$var1,input$var2,input$n1,input$n2,as.numeric(input$a1)),4)),".",sep=""),"
+            		   This indicates that there is",tags$code(sprintf("%1.2f%%",100*round(pwr(input$var1,input$var2,input$n1,input$n2,as.numeric(input$a1)),4))),"probability of detecting a difference in the variances.")
+        })
+        output$text2<- renderText({
+        	paste("Using the Contractor's Risk,",paste(tags$i(HTML("&alpha;")),",",sep=""),"of",input$a1,"with the sample size given, the risk of the State DOT not detecting a difference between the means of State DOT and Contractor's sample data when one exists ",tags$i(HTML("&beta;")),"is",paste(tags$code(1-round(pwr.t2n.test(input$n1,input$n2,d=d(input$mu1,input$mu2,input$var1,input$var2,input$n1,input$n2),as.numeric(input$a1))$power,4)),".",sep=""),"Thus the power of the test, (1-",HTML(paste(tags$i(HTML("&beta;")),")",sep="")),"is",paste(tags$code(round(pwr.t2n.test(input$n1,input$n2,d=d(input$mu1,input$mu2,input$var1,input$var2,input$n1,input$n2),as.numeric(input$a1))$power,4)),".",sep="")
+        		  ,"This indicates that there is",tags$code(sprintf("%1.2f%%",100*(round(pwr.t2n.test(input$n1,input$n2,d=d(input$mu1,input$mu2,input$var1,input$var2,input$n1,input$n2),as.numeric(input$a1))$power,4)))),"probability of detecting a difference in the means."
+        		  )
+        })
+        output$text3 <- renderUI({
+        	str <- paste("With the inputs:")
+        	str1<- paste(tags$i(HTML("&alpha;")),"=",as.numeric(input$a1))
+        	str2<- paste("The ratio of standard deviation λ=",round(sqrt(input$var1/input$var2),4))
+        	str3<- paste("The ratio of variance =",round(input$var1/input$var2,4))
+        	str4<- paste(paste("n",tags$sub("State."),sep=""),"=", input$n1)
+        	str5<- paste(paste("n",tags$sub("Contr."),sep=""),"=",input$n2)
+        	str6<- paste(tags$i(HTML("&beta;" )),"=", 1-round(pwr(input$var1,input$var2,input$n1,input$n2,as.numeric(input$a1)),4))
+        	str7<-paste("The power of test (1 -",HTML(paste(tags$i(HTML("&beta;")),")",sep="")),"is", round(pwr(input$var1,input$var2,input$n1,input$n2,as.numeric(input$a1)),4))
+        		
+        	HTML(paste(str,str1,str2,str3,str4,str5,str6,str7,sep="<br>"))
+        })
+        output$text4 <- renderUI({
+        	str <- paste("With the inputs:")
+        	str1<- paste(tags$i(HTML("&alpha;")),"=",as.numeric(input$a1))
+        	str2<- paste("The allowable difference ",paste(tags$i(HTML("d")),"*",sep=""),"=",round(d(input$mu1,input$mu2,input$var1,input$var2,input$n1,input$n2),4))
+        	str3<- paste(paste("n",tags$sub("State."),sep=""),"=", input$n1)
+        	str4<- paste(paste("n",tags$sub("Contr."),sep=""),"=",input$n2)
+        	str5<- paste(tags$i(HTML("&beta;" )),"=",1-round(pwr.t2n.test(input$n1,input$n2,d=d(input$mu1,input$mu2,input$var1,input$var2,input$n1,input$n2),as.numeric(input$a1))$power,4))
+        	str6<- paste("The power of test (1 -",HTML(paste(tags$i(HTML("&beta;")),")",sep="")),"is", round(pwr.t2n.test(input$n1,input$n2,d=d(input$mu1,input$mu2,input$var1,input$var2,input$n1,input$n2),as.numeric(input$a1))$power,4))
+        	HTML(paste(str,str1,str2,str3,str4,str5,str6,sep="<br>"))
+        })
+        output$text5<- renderUI({
+        	if (input$ar != 1){
+        		str <-paste("To achieve a power of test value of at least",input$pwr,"of being able to detect a difference between the State DOT”s and Contractor’s variances:")
+        		str1 <- paste("The required sample size for State DOT data is",tags$code(sample(as.numeric(input$a2),input$pwr,input$var3/input$var4,input$ar)[1]))
+				str2 <- paste("The required sample size for Contractor data is",tags$code(sample(as.numeric(input$a2),input$pwr,input$var3/input$var4,input$ar)[2]))
+				HTML(paste(str,str1,str2,sep="<br>"))
+        	}
+        	else {str <-paste("To achieve the selectd power value of at least",input$pwr,"to detect a difference in variances between the two data sets
+        					using equal sample sized, the required sample size for each would be",tags$code(sample(as.numeric(input$a2),input$pwr,input$var3/input$var4,input$ar)[1]))
+				HTML(paste(str))
+				}
+        })
+       
+        output$text6 <- renderUI({
+        	if (input$ar != 1) {
+        	str <- paste("To achieve a power of at least",input$pwr,"to detect a difference between the State DOT”s and Contractor’s means:")
+        	str1 <- paste("The required sample size for the State DOT data is",tags$code(n.ttest(power=input$pwr,alpha=as.numeric(input$a2),mean.diff=input$mu3-input$mu4,
+        																 sd1=sqrt(input$var3),sd2=sqrt(input$var4),k=input$ar,fraction="unbalanced")[[2]]))
+        	str2 <- paste("The required sample size for the Contractor data is",tags$code(n.ttest(power=input$pwr,alpha=as.numeric(input$a2),mean.diff=input$mu3-input$mu4,
+        																 sd1=sqrt(input$var3),sd2=sqrt(input$var4),k=input$ar,fraction="unbalanced")[[3]]))
+			HTML(paste(str,str1,str2,sep="<br>"))   
+        	}
+        	else {str <- paste("To achieve the selectd power value of at least",input$pwr,"to detect a difference in means between the two data sets
+        					using equal sample sized, the required sample size for each would be",tags$code(n.ttest(power=input$pwr,alpha=as.numeric(input$a2),mean.diff=input$mu3-input$mu4,
+        																								  sd1=sqrt(input$var3),sd2=sqrt(input$var4),k=input$ar,fraction="unbalanced")[[2]]))
+        	HTML(paste(str))
+        	}
+        })
+        
+        output$text7 <- renderUI({
+        	str <- paste("With the inputs:")
+        	str1<- paste(tags$i(HTML("&alpha;")),"=",as.numeric(input$a2))
+        	str2<- paste("The ratio of standard deviation λ=",round(sqrt(input$var3/input$var4),4))
+        	str3<- paste("The ratio of variance =",round(input$var3/input$var4,4))
+        	str4<- paste("Expected Power of test =", input$pwr)
+        	str5<- paste("Allocation ratio",paste("n",tags$sub("Contr."),"/n",tags$sub("State."),sep=""),"=",input$ar)
+        	str6<- paste("The optimized sample size of",paste("n",tags$sub("State."),sep=""),"=", sample(as.numeric(input$a2),input$pwr,input$var3/input$var4,input$ar)[1])
+        	str7<- paste("The optimized sample size of",paste("n",tags$sub("Contr."),sep=""),"=",sample(as.numeric(input$a2),input$pwr,input$var3/input$var4,input$ar)[2])
+        	HTML(paste(str,str1,str2,str3,str4,str5,str6,str7,sep="<br>"))
+        })
+        output$text8 <- renderUI({
+        	str <- paste("With the inputs:")
+        	str1<- paste(tags$i(HTML("&alpha;")),"=",as.numeric(input$a2))
+        	str2<- paste("Expected Power of test =", input$pwr)
+        	str3<- paste("Allocation ratio",paste("n",tags$sub("Contr."),"/n",tags$sub("State."),sep=""),"=",input$ar)
+        	str4<- paste("The optimized sample size of",paste("n",tags$sub("Contr."),sep=""),"=",n.ttest(power=input$pwr,alpha=as.numeric(input$a2),mean.diff=input$mu3-input$mu4,
+        														   sd1=sqrt(input$var3),sd2=sqrt(input$var4),k=input$ar,fraction="unbalanced")[[2]])
+        	str5<- paste("The optimized sample size of",paste("n",tags$sub("State."),sep=""),"=",n.ttest(power=input$pwr,alpha=as.numeric(input$a2),mean.diff=input$mu3-input$mu4,
+        														   sd1=sqrt(input$var3),sd2=sqrt(input$var4),k=input$ar,fraction="unbalanced")[[3]])
+        	HTML(paste(str,str1,str2,str3,str4,str5,sep="<br>"))
+        })
+})
---
+++ ui.R
@@ -0,0 +1,202 @@
+library(shiny)
+# Note: this app now is default for two tails test
+shinyUI(
+	fluidPage(
+		theme="bootstrap.css",			  
+		br(),
+		img(src = "FHWA.jpg",height=48,width=200),
+		titlePanel("Determination of State DOT Risk and Optimum Sample Size "),
+		br(),
+		br(),
+		navbarPage("",
+				   tabPanel("Introduction",
+				   		 h3(p("This software analysis includes four menus. In the simplest terms, one determines the State DOT’s Risk given the sample size; 
+				   		    and the second determines the sample size given the State DOT’s Risk.Two other menus - Glossary defines several terms that apply to this program and Equations provides the equations used in this program."
+				   		    ),
+				   		 	tags$ul(
+				   		 		tags$li(p("The \"State DOT's Risk\" menu determines the State DOT’s Risk (Type II Risk or",tags$i(HTML("&beta;")),"Risk) based on the Contractor’s Risk (Type I Risk or",tags$i(HTML("&alpha;")),"Risk) 
+				   		 				and the sample size n.",
+				   		 				tags$ul(
+												tags$li(p("Under the \"State DOT's Risk\" tab are three tabs, \"Summary\",", HTML(paste(tags$i("\"F-"),"test\", and",sep="")),HTML(paste(tags$i("\"t-"),"test\".",sep="")),"The summary tab provides the result of both the ", tags$i("F-"),"and ", tags$i("t-"),"test analyses.
+				   		 								The ", tags$i("F-"),"and", tags$i("t-"),"test tabs provide the details of the analysis of each of these statistical tests."
+				   		 								)),
+				   		 						tags$li(p("The State DOT's Risk,", HTML(paste(tags$i(HTML("&beta;")),",")),"is the probability of not detecting a difference between the State's and Contractor's data sets when one exists. It is desirable that this risk be as low as practical. The
+				   		 								State DOT's Risk can be adjusted by changing, either or both the Contractor's Risk,",tags$i(HTML("&alpha;")),",and sample size, n."
+				   		 								))
+				   		 						)
+				   		 				)),
+				   		 		tags$li(p("The \"Optimum Sample Size\" menu determines the Optimum Sample Size for both the", tags$i("F-"),"and",tags$i("t-"),"tests based on the Contractor’s Risk and the desired power of test value. 
+				   		 				The user has the option of using the same sample size for both the State DOT and the Contractor or different sample sizes. It is important to recognize that the sample size required 
+				   		 				for the selected risk value may be larger than anticipated.",
+				   		 				tags$ul(
+												tags$li(p("Under the \"Optimum Sample Size\" tab are three tabs, \"Summary\", ", tags$i("\"F-"),"test\", and ", tags$i("\"t-"),"test\". The summary tab provides the result of 
+				   		 								both the", tags$i("F-"),"and ", tags$i("t-"),"test analyses. The ", HTML(paste(tags$i("\"F-"),"test\" and",sep="")),HTML(paste(tags$i("\"t-"),"test\"",sep="")),"tabs provide the details of the analysis of each of these statistical tests"
+				   		 								)),
+				   		 						tags$li(p("The \"power of test\" (1-", HTML(paste(tags$i(HTML("&beta;")),")",sep="")),"is the probability of detecting a difference between the State's and Contractor's data sets when one exists."
+				   		 								)),
+				   		 						tags$li(p("The Optimum Sample Size can be adjusted by changing either or both, the Contractor's Risk,", HTML(paste(tags$i(HTML("&alpha;")),",",sep="")),"and \"power of test\" value. It is desirable to select a \"power of test\" value as high as practical."
+				   		 								)),
+				   		 						tags$li(p("It is important to recognize that the sample size, n, required for the selected \"power of test \" may be larger than anticipated.")
+				   		 						))
+				   		 				))
+				   		 		))
+				   		 		),
+				   tabPanel("State DOT's Risk",
+				   		 h3(p(
+							"In this menu, the State DOT’s risk (Type II Risk,",HTML(paste(tags$i(HTML("&beta;")),")",sep="")),"is determined. This is an important analysis because with typical sample sizes, this risk is higher than specification writers are often aware. This risk is the probability of not
+				   		    detecting a difference between the State DOT's and Contractor's data sets when one exists. (Please see more details in the \"Guidance\" tab)"
+				   		    )),
+				   		 sidebarLayout(
+				   		 	sidebarPanel(
+				   		 		h4("State DOT's Data"),
+				   		 		numericInput("mu1","Sample Mean",2.1,step=0.01),
+				   		 		numericInput("var1","Sample Variance",8.23,step=0.01),
+				   		 		numericInput("n1","Sample size",10,min=2), 
+				   		 		hr(),
+				   		 		h4("Contractor's Data"),
+				   		 		numericInput("mu2","Sample Mean",4.2,step=0.01),
+				   		 		numericInput("var2","Sample Variance",2.69,step=0.01),
+				   		 		numericInput("n2","Sample size",4,min=2),
+				   		 		hr(),
+				   		 		selectInput("a1",HTML(paste("Contractor's Risk,",tags$i(HTML("&alpha;")))),selected=0.025,c(0.01,0.025,0.05,0.1)),
+				   		 		submitButton(HTML(paste("Calculate State DOT's Risk,",tags$i(HTML("&beta;")))))
+				   		 	),
+				   		 	mainPanel(
+				   		 		tabsetPanel(
+				   		 			tabPanel("Summary",
+				   		 					 tags$ol(
+				   		 					 	h3(tags$li("Under",tags$i("F"),"test")),
+				   		 					 	h4(htmlOutput("text1"),style="color:#555555"),
+				   		 					 	tags$hr(),
+				   		 					 	h3(tags$li("Under",tags$i("t"),"test")),
+				   		 					 	h4(htmlOutput("text2"),style="color:#555555")
+				   		 					 )
+				   		 			),
+				   		 			tabPanel(HTML(paste(tags$i("F"),"test")),
+				   		 					 h4(htmlOutput("text3"),style="color:#555555")
+				   		 			),
+				   		 			tabPanel(HTML(paste(tags$i("t"),"test")),
+				   		 					 h4(htmlOutput("text4"),style="color:#555555"),
+				   		 					 h6("*The allowable difference",tags$i("d"),"is the difference between the State DOT’s and Contractor’s means divided by their pooled standard deviation.",style="color:#555555")
+				   		 			),
+				   		 			tabPanel("Guidance",
+				   		 					 h4(
+				   		 					 	tags$ul(tags$li(p("The State DOT's Risk is based on the Contractor's Risk and a selected sample size, n. A low value of", tags$i(HTML("&beta;")),"is desirable.")),
+				   		 					 			tags$li(p("It is important to know the interrelationship among",tags$i(HTML("&alpha;,")),tags$i(HTML("&beta;,")),"and sample size, n. A well-written acceptance plan attempts to provide a balance between",tags$i(HTML("&alpha;")),"and",tags$i(HTML("&beta;.")),"If the FHWA 
+				   		 					 					suggested value of 0.025 is used as the ",tags$i(HTML("&alpha;")),"risk, it may be difficult to obtain a relatively low value for",tags$i(HTML("&beta;")),"unless a large sample size is used. In the event the State DOT Risk is considered too high, and the desired balance of
+				   		 					 					the risks between the Contractor and the State DOT is not obtained, it is not realistic to increase the Contractor's risk,",tags$i(HTML("&alpha;,")),"to more than about 5%. If the ",tags$i(HTML("&alpha;")),"risk is limited to an upper value of 5% and the analysis indicates the
+				   		 					 					State DOT's risk,",tags$i(HTML("&beta;,")),"is too high, the only alternative is to increase the sample size."),
+				   		 					 					tags$ul(
+				   		 					 						tags$li(p("A subjective acceptable range of Contractor's risk, ",tags$i(HTML("&alpha;,")),"is 1% (0.01) to 5.0% (0.05). The range is even more subjective for the State DOT's risk,",tags$i(HTML("&alpha;,")),"than for the Contractor's risk because of the above mentioned relationship
+				   		 					 								among ",tags$i(HTML("&alpha;,")),tags$i(HTML("&beta;,")),"and sample size, n. The question becomes, how certain does the State DOT want to be that they are not verifying Contractor's data erroneously. Based on our experience, our recommendations are: a low",tags$i(HTML("&beta;")),"risk may be considered to be 30% (0.30) to 
+				   		 					 								40% (0.40); a medium",tags$i(HTML("&beta;")),"risk may be considered to be 50% (0.50) to 60% (0.60); and any",tags$i(HTML("&beta;")),"risk higher than 60% (0.60) may be considered higher than desirable.")
+				   		 					 								)
+				   		 					 							)
+				   		 					 					)
+				   		 					 			)
+				   		 					 	)
+				   		 			)
+				   		 		)
+				   		 	)
+				   		 )),
+				   tabPanel("Optimum Sample Size",
+				   		 h3(p("In this menu, the optimum sample size needed for a given power of test value is calculated based on the State DOT’s and 
+				   		 			  Contractor’s statistical parameters and the Contractor's Risk,",HTML(paste(tags$i(HTML("&alpha;")),".",sep="")),"The menu allows different sample sizes for the State DOT’s and Contractor’s 
+				   		 			  data; this is called the “Allocation ratio”,",HTML(paste("n",tags$sub("Contr."),sep=""),"/",paste("n",tags$sub("State."),".",sep=""))
+				   		 	 )),
+				   		 sidebarLayout(
+				   		 	sidebarPanel(
+				   		 		h4("State DOT's Data"),
+				   		 		numericInput("mu3","Sample Mean",13.2),
+				   		 		numericInput("var3","Sample Variance",1.22),
+				   		 		hr(),
+				   		 		h4("Contractor's Data"),
+				   		 		numericInput("mu4","Sample Mean",14.3),
+				   		 		numericInput("var4","Sample Variance",1.76),
+				   		 		hr(),
+				   		 		numericInput("pwr",HTML(paste("Expected power of test (1 -",tags$i(HTML("&beta;)")))),0.9,min=0,max=1,step=0.01), 
+				   		 		numericInput("ar",HTML(paste("n",tags$sub("Contr."),sep=""),"/",paste("n",tags$sub("State."),sep="")),1,step=0.1),
+				   		 		selectInput("a2",HTML(paste("Contractor's Risk,",tags$i(HTML("&alpha;")))),selected=0.025,c(0.01,0.025,0.05,0.1)),
+				   		 		submitButton("Calculate Optimum Sample Size")
+				   		 	),
+				   		 	mainPanel(
+				   		 		tabsetPanel(
+				   		 			tabPanel("Summary",
+				   		 					 tags$ol(
+				   		 					 	h3(tags$li("Under",tags$i("F") ,"test")),
+				   		 					 	h4(htmlOutput("text5"),style="color:#555555"),
+				   		 					 	tags$hr(),
+				   		 					 	h3(tags$li("Under",tags$i("t"),"test")),
+				   		 					 	h4(htmlOutput("text6"),style="color:#555555")
+				   		 					 )
+				   		 			),
+				   		 			tabPanel(HTML(paste(tags$i("F"),"test")),
+				   		 					 h4(htmlOutput("text7"),style="color:#555555")),
+				   		 			tabPanel(HTML(paste(tags$i("t"),"test")),
+				   		 					 h4(htmlOutput("text8"),style="color:#555555")),
+				   		 			tabPanel("Guidance",
+				   		 					 h3(tags$ul(tags$li("If the required sample sizes are considered to be too large to be practical, there are some adjustments that can be made to the inputs. The Contractor’s Risk can be increased, 
+											or the power of test value can be decreased. A third choice may be the option to delay a decision concerning verification until the desired sample sizes are reached.
+				   		 					 				   ")
+				   		 					 )
+				   		 					 ))
+				   		 		)
+				   		 	)
+				   		 )),
+				   tabPanel("Glossary",
+				   		 h4(
+				   		 		tags$p(tags$b("Risks:"),"The probability or chance of making the wrong decision in a statistical acceptance plan or test for verification. Risks are strongly influenced by the sample size, n."),
+				   		 		
+				   		 		tags$p(tags$b("Alpha (",tags$i(HTML("&alpha;")),") risk:"),"Also called Contractor's risk, risk of a Type I, or alpha error. Related to verification, this is the risk to the Contractor of having statistical data judged not to come from the same population as the State DOT statistical data when it actually does. This risk is often set at a range 0.01 - 0.05 suggested by FHWA."),
+				   		 		
+				   		 		tags$p(tags$b("Beta (",(tags$i(HTML("&beta;"))),") risk:"),"Also called State DOT's risk, risk of a Type II, or beta error. Related to verification, this is the risk to the State DOT of having statistical data judged to come from the same population as the Contractor’s statistical data from it when actually does not. This risk should be kept as low as practical."),
+				   		 		tags$p(tags$b("Interrelationship among ",HTML(paste(tags$i(HTML("&alpha;")),",",sep=""),paste(tags$i(HTML("&beta;")),",",sep="")),"and sample size, n:"),"A well-written acceptance plan attempts to provide a balance between α and β. If the FHWA suggested value range of 0.01 - 0.05 is used, it may be difficult to obtain a relatively low value for β unless a large sample size is used."),
+				   		 		tags$p(tags$b("Power of test ( 1-",(tags$i(HTML("&beta;"))),"):"),"This is the probability of being able to detect a difference between the Contractor’s statistical data and the State DOT’s data when one exits. It is desirable that the power of the test be as high as practical.")
+				   		 	
+				   		 )),
+				   tabPanel("Equations",
+				   		 h2("Equations",align="center"),
+				   		 withMathJax(),# \\(...\\) represents inlinemath, \\[...\\] or $$...$$ represents displaymath
+				   		 tags$ul(
+				   		 	tags$li(h4(
+				   		 		p(tags$b(tags$i("F-"),"test:")),
+				   		 		p("$$F=s^2_{c}/s^2_{a}\\text{ or }s^2_{a}/s^2_{c}$$"),
+				   		 		p("Where:"),
+								p("\\(s^2_{c}=\\text{variance of Contractor's data}\\)",style="text-indent:50px"),
+				   		 		p("\\(s^2_{a}=\\text{variance of State DOT's data}\\)",style="text-indent:50px")
+								 )),
+				   		 	tags$li(h4(
+				   		 		p(tags$b(tags$i("t-"),"test:")),
+				   		 		p("$$t=\\frac{\\left|\\bar{X}_{c}-\\bar{X}_{a} \\right|}{\\sqrt{\\frac{s^2_{p}}{n_{c}}+\\frac{s^2_{p}}{n_{a}}}}$$"),
+				   		 		p("Where:"),
+				   		 		p("\\(\\bar{X}_{c}=\\text{mean of Contractor's data}\\)",style="text-indent:50px"),
+				   		 		p("\\(\\bar{X}_{a}=\\text{mean of State DOT's data}\\)",style="text-indent:50px"),
+				   		 		p("\\(s^2_{p}=\\text{pooled variance}\\)",style="text-indent:50px"),
+				   		 		p("\\(n_{c}=\\text{degrees of freedom of Contractor’s data}\\)",style="text-indent:50px"),
+				   		 		p("\\(n_{a}=\\text{degrees of freedom of State DOT’s data}\\)",style="text-indent:50px")
+				   		 		  )),
+							tags$li(h4(
+									   p(tags$b("Pooled variance:")),
+									   p("$$s^2_{p}=\\frac{s^2_{c}(n_{c}-1)+s^2_{a}(n_{a}-1)}{n_{c}+n_{a}-2}$$"),
+									   p("Where:"),
+									   p("\\(s^2_{p}=\\text{pooled variance}\\)",style="text-indent:50px"),
+									   p("\\(s^2_{c}=\\text{variance of Contractor's data}\\)",style="text-indent:50px"),
+									   p("\\(s^2_{a}=\\text{variacen of State DOT's data}\\)",style="text-indent:50px"),
+									   p("\\(n_{c}=\\text{degrees of freedom of Contractor's data}\\)",style="text-indent:50px"),
+									   p("\\(n_{a}=\\text{degrees of freedom of State DOT's data}\\)",style="text-indent:50px")
+									   )),
+							tags$li(h4(
+								p(tags$b("Allowable difference, d:")),
+								p("$$d=\\frac{\\mu_{c}-\\mu_{a}}{s_{d}}$$"),
+								p("Where:"),
+								p("\\(d=\\text{allowable difference between two means}\\)",style="text-indent:50px"),
+								p("\\(\\mu_{c}=\\text{mean of Contractor's data}\\)",style="text-indent:50px"),
+								p("\\(\\mu_{a}=\\text{mean of State DOT's data}\\)",style="text-indent:50px"),
+								p("\\(s_{d}=\\text{standard deviation of the difference}\\)",style="text-indent:50px")
+							))
+								)
+							),
+				   tabPanel("About")
+				   		 )
+				   )
+)
